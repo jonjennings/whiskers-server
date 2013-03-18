9e1c410fe81b63ec0f99ab56df673103851123e4
jonjennings <github@mlist.ca>
2013-03-17 22:00:26 -0700

    Make a note: we can't currently use CI's CSRF protection because we don't let CI generate our forms for us

--- whiskers/config/config.php
+++ whiskers/config/config.php
@@ -293,6 +293,9 @@ $config['global_xss_filtering'] = TRUE;
 | 'csrf_cookie_name' = The cookie name
 | 'csrf_expire' = The number in seconds the token should expire.
 */
+/* Whiskers note: we can't currently use CSRF protection as we don't let
+ * CI generate our forms for us
+ */
 $config['csrf_protection'] = FALSE;
 $config['csrf_token_name'] = 'csrf_test_name';
 $config['csrf_cookie_name'] = 'csrf_cookie_name';
