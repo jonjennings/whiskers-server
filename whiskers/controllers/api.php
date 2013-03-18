5dd1b7a185cc7639f8ad3b064c541fd856024ca2
jonjennings <github@mlist.ca>
2013-03-17 23:28:55 -0700

    Server-side routes incoming AJAX calls from browser to each appropriate service driver and also writes successfully posted posts to the Whiskers database

--- whiskers/controllers/api.php
+++ whiskers/controllers/api.php
@@ -15,6 +15,7 @@ class Api extends CI_Controller {
         $this->load->library('whiskers_user');
         $this->load->library('whiskers_db', array('settings'), 'settings');
         $this->load->library('whiskers_db', array('posts'), 'posts');
+        $this->load->library('whiskers_db', array('service_posts'), 'service_posts');
         $this->load->driver('whiskers_post');
     }
 
@@ -40,25 +41,74 @@ class Api extends CI_Controller {
 
     /**
      * Saves a whisker post.
-     *
-     * @param   string  $driver     name of the driver
-     * @param   string  $text       text of the post
+     * 
+     * Calls from the client come here.
+     * One call for each service to which the post must be sent
+     * 
+     * timestamp & parent_text params are common across all service-specific
+     * instances of a given post
+     * 
+     * @param   string  $driver     	name of the driver
+     * @param   string  $text       	text of the post
+     * @param   string  $timestamp  	seconds since Unix Epoch
+     * @param   string  $parent_text	text of the parent post
+     * 
      * @return  object
      */
     function post_post()
     {
         $this->data['driver'] = $this->input->post('driver');
         $this->data['text'] = $this->input->post('text');
+        $this->data['timestamp'] = $this->input->post('timestamp');
+        $this->data['parent_text'] = $this->input->post('parent_text');
 
         $driver = $this->input->post('driver');
 
-		// This calls save_post() in the appropriate service's driver
+		// This calls save_post() in the appropriate service's driver to post to the service
         if ( ! $data = $this->whiskers_post->$driver->save_post($this->data['text']))
         {
-            $this->data['message'] = "Failed to post to {$this->data['driver']}.";
+            $this->data['message'] = "Failed to post to $driver.";
             $this->_respond(500);
-        }
-        
+        } else {
+			// Post to service was successful
+			// So now save post to the database
+
+			// This key is common to all service-specific instances of this post
+			$key = sha1($this->input->post('timestamp').':'.$this->input->post('parent_text'));
+			
+			// Add the post (or update it) to the posts table.
+			// This is the common entry that links the individual service-specific posts
+			// as such, we store the parent_text in here
+			$saved = $this->posts->update($key, array(
+				'type' => 'post',
+				'text' => $this->input->post('parent_text'),
+				'time' => time(),		// Don't trust the client data - use server-side time rather than client's timestamp
+				'source_urls' => array(
+					$driver => $data
+				)
+			));
+			
+			// Add put the post into the service posts table
+			// Note there may be multiple posts in here with the same key
+			// The key ties these service specific postings back to the parent
+			// post in the main posts table
+			$saved = $this->service_posts->update($key, array(
+				'type' => 'post',
+				'text' => $this->input->post('text'),
+				'time' => time(),		// Don't trust the client data - use server-side time rather than client's timestamp
+				'source_urls' => array(
+					$driver => $data
+				)
+			), true);
+
+			
+			
+			if (!$saved) {
+				$this->data['message'] = "Failed to save post to $driver in database.";
+				$this->_respond(500);
+			}
+		}
+
         $this->_respond(200);
     }
 
